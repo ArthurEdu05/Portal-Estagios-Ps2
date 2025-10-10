13db248d36d6fbcaa7e74a3266977359e88fddee
Arthur Eduardo <163680207+ArthurEdu05@users.noreply.github.com>
2025-10-10 11:40:25 +0000

    Initialize Spring Boot Project

---
+++ src/test/java/br/mackenzie/ps2/portalestagios/PortalestagiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.ps2.portalestagios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalestagiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
