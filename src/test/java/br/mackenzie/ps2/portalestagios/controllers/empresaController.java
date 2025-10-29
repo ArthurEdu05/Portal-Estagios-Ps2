c66c1f2bf492b5d5ef1bd1ec00a32c810218180a
ArthurEdu05 <arthuredu2005@gmail.com>
2025-10-29 19:49:29 -0300

    feat(controllers): implement 'Empresa' and 'Estudante' logic

--- src/test/java/br/mackenzie/ps2/portalestagios/controllers/empresaController.java
+++ src/test/java/br/mackenzie/ps2/portalestagios/controllers/empresaController.java
@@ -1,14 +1,77 @@
 package br.mackenzie.ps2.portalestagios.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.mackenzie.ps2.portalestagios.entities.Empresa;
+import br.mackenzie.ps2.portalestagios.repository.empresaRepository;
+
+@RestController
 public class empresaController {
+
+    @Autowired
+    private empresaRepository empresaRep;
+    List<Empresa> lstEmpresas = new ArrayList<>();
 
     //CRUD - EMPRESA
 
     //CREATE
+    @PostMapping("/empresa")
+    public Empresa createEmpresa(@RequestBody Empresa newEmpresa){
+        if(newEmpresa.getNome() == null || newEmpresa.getCnpj() == null || newEmpresa.getEmail() == null 
+        || newEmpresa.getNome().isEmpty() || newEmpresa.getCnpj().isEmpty() || newEmpresa.getEmail().isEmpty()){
+            
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        
+        }
 
+        return empresaRep.save(newEmpresa);
+    }
     //READ
+    @GetMapping("/empresa")
+    public List<Empresa> getAllEmpresas(){
+        return empresaRep.findAll();
+    }
 
     //UPDATE
+    @PutMapping("/empresa/{id}")
+    public Empresa updateEmpresaById(@RequestBody Empresa newData, @PathVariable Long id){
+        Optional<Empresa> optional = empresaRep.findById(id);
+        if(optional.isPresent()){
+            Empresa newEmpresa = optional.get();
+            newEmpresa.setNome(newData.getNome());
+            newEmpresa.setCnpj(newData.getCnpj());
+            newEmpresa.setEmail(newData.getEmail());
+            return empresaRep.save(newEmpresa);
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+    }
 
     //DELETE
+    @DeleteMapping("/empresa/{id}")
+    public Empresa deleteEmpresaById(@PathVariable Long id){
+        Optional<Empresa> optional = empresaRep.findById(id);
+        if(optional.isPresent()){
+            Empresa del = optional.get();
+            empresaRep.deleteById(id);
+            return del;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+    }
 }
--- src/test/java/br/mackenzie/ps2/portalestagios/controllers/estudanteController.java
+++ src/test/java/br/mackenzie/ps2/portalestagios/controllers/estudanteController.java
@@ -1,15 +1,74 @@
 package br.mackenzie.ps2.portalestagios.controllers;
 
-public class estudanteController {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.mackenzie.ps2.portalestagios.repository.estudanteRepository;
+import br.mackenzie.ps2.portalestagios.entities.Estudante;
+
+@RestController
+public class estudanteController {
+    @Autowired
+    private estudanteRepository estudanteRep;
+    List<Estudante> lstEstudantes = new ArrayList<>();
 
     //CRUD - ESTUDANTE
 
     //CREATE
+    @PostMapping("/estudante")
+    public Estudante createEstudante(@RequestBody Estudante newEstudante){
+        if(newEstudante.getNome() == null || newEstudante.getCpf() == null || newEstudante.getEmail() == null || newEstudante.getListAreaInteresse() == null
+        || newEstudante.getNome().isEmpty() || newEstudante.getCpf().isEmpty() || newEstudante.getEmail().isEmpty() || newEstudante.getListAreaInteresse().isEmpty()){
+            
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        return estudanteRep.save(newEstudante);
+    }
 
     //READ
+    @GetMapping("/estudante")
+    public List<Estudante> getAllEstudantes(){
+        return estudanteRep.findAll();
+    }
 
     //UPDATE
+    @PutMapping("/estudante/{id}")
+    public Estudante updateEstudanteById(@RequestBody Estudante newData, @PathVariable Long id){
+        Optional<Estudante> optional = estudanteRep.findById(id);
+        if(optional.isPresent()){
+            Estudante newEstudante = optional.get();
+            newEstudante.setNome(newData.getNome());
+            newEstudante.setCpf(newData.getCpf());
+            newEstudante.setEmail(newData.getEmail());
+            newEstudante.setListAreaInteresse(newData.getListAreaInteresse());
+        }
 
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+    }
     //DELETE
+    @DeleteMapping("/estudante/{id}")
+    public Estudante deleteEstudanteById(@PathVariable Long id){
+        Optional<Estudante> optional = estudanteRep.findById(id);
+        if(optional.isPresent()){
+            Estudante del = optional.get();
+            estudanteRep.deleteById(id);
+            return del;
+        }
+
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+    }
 }
