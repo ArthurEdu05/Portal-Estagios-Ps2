2e6250f029744baa136660563e1eb43e0f456747
ArthurEdu05 <arthuredu2005@gmail.com>
2025-12-04 11:32:10 -0300

    final version

--- src/main/java/br/mackenzie/ps2/portalestagios/controllers/administradorController.java
+++ src/main/java/br/mackenzie/ps2/portalestagios/controllers/administradorController.java
@@ -1,9 +1,7 @@
 package br.mackenzie.ps2.portalestagios.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
